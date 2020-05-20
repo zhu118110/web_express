e7e00bc9cb143933c441b95c06663dd9dd5565da
admin <15135521040@163.com>
2020-05-20 17:56:30 +0800

    修改存储头像接口

--- routes/aboutMe.js
+++ routes/aboutMe.js
@@ -32,15 +32,16 @@ router.post("/aboutMe",function(req,res){
     })
 })
 
-let imgName=""
+
 
 // 我的资料 页面，修改头像时接收客户端提交的头像并改变数据库的头像
 router.post("/uploadHeadImg",function(req,res){
+    let userId ="";
     var form = new formidable.IncomingForm();
     form.encoding = 'utf-8';
     form.maxFilesSize = 2 * 1024 * 1024;
     form.keepExtensions=true; //保留后缀
-    form.uploadDir="public/temp";   //设置临时目录
+    form.uploadDir="public/uploadHeadImages";   //设置临时目录
     form.parse(req,function(err,fields,files){
         if(err){
             console.log("没有接收到图片"+err);
@@ -48,61 +49,42 @@ router.post("/uploadHeadImg",function(req,res){
                 code:"0"
             })
         }
+        userId=fields._id;  //获取用户id
         // files 包括接受的文件的信息
         Object.keys(files).forEach(function(name) {  
             // console.log(name);   //name是客户端上传的文件路径
-           
             // console.log(files[name].path); // 服务端保存的路径
-            imgName=files[name].path.slice(12);
-           
-            res.json({
-                code:"1",
-                path:files[name].path,
+            // 将上传的图像保存到数据
+            let filePath="http://192.168.88.69:3200";
+            filePath+=files[name].path.substring(6);  //将返回的临时地址中的 public\ 字段删掉改为服务端地址
+            filePath=filePath.replace(/\\/img,"/");    //将路径中的\变为/
+            
+            // 更新头像字段
+            tb_regist.findByIdAndUpdate({"_id":userId},{"$set":{
+                "headImg":filePath
+            }},function(err,doc){
+                console.log(doc);
+                if(err&&!doc){
+                    res.json({
+                        code:"0"
+                    })
+                }else{
+                    res.json({
+                        code:"1",
+                    })
+                }
+
             })
+
+
+           
         });
     }) 
 }),
 
 
+// 编辑个人资料
 router.post("/editUserName",function(req,res){
-    let temp="E:/muiApp/mui_express/public/temp/";   //本地临时文件夹
-	let imgArr=[];
-    fs.readdir(temp,function(err,files){
-        if(err){
-            res.json({
-                code:"0"
-            });
-            return false;
-        }else{
-            // files:[]
-            for(let i in files){
-                imgArr.push(files[i])
-                console.log(files[i])
-                    var read=fs.createReadStream(temp+files[i]);  //读取默认目录下的图片
-                    var newPath='/uploadHeadImages/'+files[i];   //创建保存图片的新的文件路径
-                    var write=fs.createWriteStream("public/"+newPath);   //设置新的路径
-                    read.pipe(write); 
-                    // 写入完成后执行
-                    write.on('close',function (err) { 
-                        if(err){
-                            throw err;
-                        }else{
-                            // 清空临时文件夹
-                            fs.unlink(temp+files[i],function(err){
-                                if(err) throw err;
-                                console.log("删除成功")
-                            })
-                        }
-                    })
-                
-                
-            }
-        }
-    })
-
-
-
-
     let [_id,userName,headImg]=[req.body._id,req.body.userName,req.body.headImg];
     tb_regist.findByIdAndUpdate({_id},{"$set":{
         userName,
